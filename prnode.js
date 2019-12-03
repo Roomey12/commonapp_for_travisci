c34461b37bb027cf56848d651c1573d2d56f9058
Roomey12 <54270364+Roomey12@users.noreply.github.com>
2019-12-03 14:42:59 +0200

    add lab file

--- prnode.js
+++ prnode.js
@@ -1,23 +1,13 @@
-function Mail() {
-  let re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
-  let myMail = document.getElementById('email').value;
-  let valid = re.test(myMail);
-  if (valid) alert('Адрес эл. почты введен правильно!');
-  else alert('Адрес электронной почты введен неправильно!');
-}
+'use strict';
 
-function Phone() {
-  let re = /^\d[\d\(\)\ -]{8,14}\d$/;
-  let myPhone = document.getElementById('phone').value;
-  let valid = re.test(myPhone);
-  if (valid) alert('Номер телефона введен правильно!');
-  else alert('Номер телефона введен неправильно!');
-}
-
-function Name() {
-  let re = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']$/;
-  let myName = document.getElementById('name1').value;
-  let valid = re.test(myName);
-  if (valid) alert('Имя введено правильно!');
-  else alert('Имя введено неправильно!');
-}
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hi there!\n');
+});
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
