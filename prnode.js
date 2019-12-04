886351a6bb390767e5d3a136b788780276ae5aa3
Roomey12 <54270364+Roomey12@users.noreply.github.com>
2019-12-04 10:38:40 +0200

    herokufix

--- prnode.js
+++ prnode.js
@@ -2,12 +2,13 @@
 
 const http = require('http');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = server.listen(process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hi there!\n');
 });
+
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
